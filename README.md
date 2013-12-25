cc8d61d32c0903af726b7a70af1cf86bd73b37ad
Andrew Ferguson <adferguson@gmail.com>
2013-12-25 09:49:15 -0500

    deletions for flowlog branch

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-Welcome to the PANE Demo
-========================================================
 
-This VM is configured to with example programs and demos to illustrate the
-promise of Participatory Networking.
-
-To get started:
-
-1. Try out some scripts in the demos directory. For example:
-    * `sudo demos/PaneDemo.py` launches a simple network with a PANE controller.
-    * `sudo demos/ZooKeeperDemo.py` launches a PANE-enabled ZooKeeper ensemble.
-These will silently launch a PANE controller within the demo.
-
-2. To run PANE separately: `./pane/pane -c etc/pane.cfg -p 4242` and start
-   demos with `--controller=remote`.
-
-Both PANE and the demos display help with a `--help` option.
--- bin/start-ovs.sh
+++ bin/start-ovs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo modprobe openvswitch_mod
+sudo modprobe openvswitch
 
-sudo ovsdb-server /usr/local/etc/ovs-vswitchd.conf.db --remote=punix:/usr/local/var/run/openvswitch/db.sock --detach
+sudo ovsdb-server /usr/local/etc/openvswitch/ovs-vswitchd.conf.db -vANY:CONSOLE:EMER -vANY:SYSLOG:ERR -vANY:FILE:INFO --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,manager_options --private-key=db:SSL,private_key --certificate=db:SSL,certificate --bootstrap-ca-cert=db:SSL,ca_cert --no-chdir --log-file=/var/log/openvswitch/ovsdb-server.log --pidfile=/usr/local/var/run/openvswitch/ovsdb-server.pid --detach --monitor
 
-sudo ovs-vswitchd --detach unix:/usr/local/var/run/openvswitch/db.sock
+sudo ovs-vswitchd unix:/usr/local/var/run/openvswitch/db.sock -vANY:CONSOLE:EMER -vANY:SYSLOG:ERR -vANY:FILE:INFO --mlockall --no-chdir --log-file=/var/log/openvswitch/ovs-vswitchd.log --pidfile=/usr/local/var/run/openvswitch/ovs-vswitchd.pid --detach --monitor
